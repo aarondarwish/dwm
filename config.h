db1eaaf3b215b0b979448473e7c8c64cb210e52e
Aaron Darwish <aaron7darwish@gmail.com>
2021-12-27 15:03:23 +0000

    By default, there will be a gap even if only one window is opened.

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "Mono:bold:pixelsize=18", "JoyPixels:bold:pixelsize=18:antialias=true:autohint=true"  };
